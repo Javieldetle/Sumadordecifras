bb032b67e3297957eb9b5e75971cd234d8310f90
Javier <javieldetle@hotmail.com>
2022-04-12 22:06:06 +0200

    test

--- ASumar/src/sumador/ASumarTest.java
+++ ASumar/src/sumador/ASumarTest.java
@@ -7,8 +7,26 @@ import org.junit.Test;
 public class ASumarTest {
 
 	@Test
-	public void testTotal() {
-		fail("Not yet implemented");
+	public void testUnaCifra() {
+		ASumar sum1= new ASumar();
+		int resultado = sum1.total("5");
+		assertEquals("5=5", resultado);
+		ASumar sum2= new ASumar();
+		int resultado2 = sum2.total("12");
+		assertEquals("eso no es un digito ", resultado2);
+	}
+	@Test
+	public void testNegativo() {
+		ASumar suma= new ASumar();
+		int resultado = suma.total("-5");
+		assertEquals("Negativo.Siempre negativo ", resultado);
+	}
+	
+	@Test
+	public void concatenaYSuma() {
+		ASumar suma= new ASumar();
+		int resultado = suma.total("15");
+		assertEquals(6, resultado);
 	}
 
 }
